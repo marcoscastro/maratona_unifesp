2e08cfb015f67c998f11b824fe586153f7b06b0c
marcos <marcos@marcos-not.(none)>
2015-06-18 04:48:42 -0300

    solucao para o problema do URI 1037

---
+++ URI-1037/1037_marcoscastro.cpp
@@ -0,0 +1,25 @@
+/*
+	Solução para o problema:
+	https://www.urionlinejudge.com.br/judge/en/problems/view/1037
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	float n;
+	
+	scanf("%f", &n);
+	if(n >= 0 && n <= 25)
+		printf("Intervalo [0,25]\n");
+	else if(n > 25 && n <= 50)
+		printf("Intervalo (25,50]\n");
+	else if(n > 50 && n <= 75)
+		printf("Intervalo (50,75]\n");
+	else if(n > 75 && n <= 100)
+		printf("Intervalo (75,100]\n");
+	else
+		printf("Fora de intervalo\n");
+	 
+	return 0;
+}
